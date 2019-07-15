18455ff5a7af1358f1ee8719f7967048bb88775f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-07-15 11:53:45 -0700

    👌 IMPROVE: Filter My Sites
    Move Site Filtering from Admin Bar to get_blogs_of_user filter
    
    Allows My Sites page display accurate information, as well as
    any other areas where a list of sites is used

---
+++ modules/my-sites.php
@@ -0,0 +1,50 @@
+<?php
+
+add_filter( 'get_blogs_of_user', 'bc_custom_get_blogs_filter', 10, 3 );
+
+function bc_custom_get_blogs_filter( $sites, $user_id, $all ) {
+
+	// Transient Name
+	$transient_name = 'bc-my-sites-' . wp_get_current_user()->user_login;
+
+	// This is a heavy function, so we want to cache it using a transient
+	$sites_cached = get_site_transient( $transient_name );
+
+	if ( ! current_user_can( 'manage_network' ) && is_multisite() ) {
+
+		// Is there anything in the cache?
+		if ( false === $sites_cached ) {
+
+			foreach ( (array) $sites as $key => $site ) {
+
+				// Query site user table to see if user has a non-Subscriber role
+				$props = array(
+					'blog_id'      => $site->userblog_id,
+					'role__in'     => array(
+											'author',
+											'contributor',
+											'dept-site-owner',
+											'editor',
+											'gf-view-entries',
+											'calendar-contributor',
+										),
+					'include'      => array( get_current_user_id() ),
+					'fields'       => array( 'ID' ),
+				);
+				$users = get_users( $props );
+
+				// Remove sites without the user from the sites array
+				if ( ! isset( $users[0] ) ) {
+					unset( $sites[ $key ] );
+				}
+			}
+
+			// Cache this in a transient for 3 seconds
+			set_site_transient( $transient_name, $sites, 3 );
+
+			return $sites;
+		}
+	}
+
+	return $sites_cached;
+}
