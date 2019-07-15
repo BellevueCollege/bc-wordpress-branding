047d98aa7fdfcf1fcb035163e735a3a38d68fee7
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-07-15 12:04:21 -0700

    👌 IMPROVE: Adjust Caching of Site List

--- modules/my-sites.php
+++ modules/my-sites.php
@@ -4,17 +4,17 @@ add_filter( 'get_blogs_of_user', 'bc_custom_get_blogs_filter', 10, 3 );
 
 function bc_custom_get_blogs_filter( $sites, $user_id, $all ) {
 
-	// Transient Name
-	$transient_name = 'bc-my-sites-' . wp_get_current_user()->user_login;
-
-	// This is a heavy function, so we want to cache it using a transient
-	$sites_cached = get_site_transient( $transient_name );
-
-	if ( ! current_user_can( 'manage_network' ) && is_multisite() ) {
+	if ( ! current_user_can( 'manage_network' ) &&  is_multisite() ) {
+	
+		// Transient Name
+		$transient_name = 'bc-my-sites-' . wp_get_current_user()->user_login;
+	
+		// This is a heavy function, so we want to cache it using a transient
+		$sites_cached = get_site_transient( $transient_name );
 
 		// Is there anything in the cache?
 		if ( false === $sites_cached ) {
-
+		
 			foreach ( (array) $sites as $key => $site ) {
 
 				// Query site user table to see if user has a non-Subscriber role
@@ -27,7 +27,7 @@ function bc_custom_get_blogs_filter( $sites, $user_id, $all ) {
 											'editor',
 											'gf-view-entries',
 											'calendar-contributor',
-										),
+										),    
 					'include'      => array( get_current_user_id() ),
 					'fields'       => array( 'ID' ),
 				);
@@ -41,10 +41,9 @@ function bc_custom_get_blogs_filter( $sites, $user_id, $all ) {
 
 			// Cache this in a transient for 3 seconds
 			set_site_transient( $transient_name, $sites, 3 );
-
 			return $sites;
 		}
+		return $sites_cached;
 	}
-
-	return $sites_cached;
+	return $sites;
 }
